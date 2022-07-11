acc165eb29be1084f75081b1f9b4f369f509227b
Madi Greeley <98246671+Madi-Feil@users.noreply.github.com>
2022-07-11 00:18:27 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+function App() {
+  return (
+      <>
+          <h1>Hello Dojo!</h1>
+          <h3>Things I need to do:</h3>
+          <p>*Learn React</p>        
+          <p>*Climb Mt. Everest</p>
+          <p>*Run a marathon</p>
+          <p>*Feed the dogs</p>
+      </>
+  );
+}
+
+export default App;
